bbc5dc07772efb10e17f1c00dcc8e0bf173cc8f4
Brian Kimosop <90181656+bkimosop@users.noreply.github.com>
2024-02-27 14:26:42 +0300

    Python week 1

---
+++ user_input.py
@@ -0,0 +1,12 @@
+>>> name = input("Please enter your name: ")
+Please enter your name: brian
+>>> print(f"Hello, {name}! Nice to meet you.")
+Hello, brian! Nice to meet you.
+>>> age = input("Please enter your age: ")
+Please enter your age: 24
+>>> location = input("Please enter your location: ")
+Please enter your location: Kenya
+>>> print(f"You are located in: {location}.")
+You are located in: Kenya.
+>>> print(f"Hello {name}, you are {age} years old and live in {location}.")
+Hello brian, you are 24 years old and live in Kenya.
